612681ee3e5e2da2f3573134fd688ba49aca269d
real_sohel <salmanisohel26@gmail.com>
2023-12-23 18:33:59 +0530

    Created the home page's main component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Main from '../components/main'
 
 const Home = () => {
     return (
         <div className=''>
-            hellp
+            <Main/>
         </div>
     )
 }
