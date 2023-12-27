3534d35c9f35394435b1dd2b877688c11dcbf3f4
real_sohel <salmanisohel26@gmail.com>
2023-12-27 19:09:07 +0530

    Created Row and scrollable

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Main from '../components/main'
+import Row from '../components/row'
+import requests from '../request'
+
 
 const Home = () => {
     return (
         <div className=''>
             <Main/>
+            <Row rowID='1' title='Trending' fetchUrl={requests.requestTrending}/>
+            <Row rowID='2' title='UpComing' fetchUrl={requests.requestUpcoming}/>
+            <Row rowID='3' title='Top Rated' fetchUrl={requests.requestTopRated}/>
+            <Row rowID='4' title='Popular' fetchUrl={requests.requestPopular}/>
+            <Row rowID='5' title='Horror' fetchUrl={requests.requestHorror}/>
         </div>
     )
 }
